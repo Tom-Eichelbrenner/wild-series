4d1458e1ea3e3e2c638a2a39d4f8e9cf69c7dc06
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2020-06-02 16:37:36 +0200

    Fucking templates.

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\CategoryAddType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * @Route("/category", name="category_")
+ *
+ */
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(Request $request): Response
+    {
+        $form = $this->createForm(CategoryAddType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $category = $form->getData();
+            $em->persist($category);
+            $em->flush();
+        }
+
+        return $this->render('category/add.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
